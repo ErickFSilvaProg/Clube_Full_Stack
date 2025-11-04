c37b0d08f493f95e409c1e3bdc1189a931077242
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-11-04 16:40:08 -0300

    Clube Full Stack, 04/11/2025 (tarde)

--- secao01-php-para-quem-nao-sabe-php/aula003-variaveis-tipos-de-dados-referencia/index.php
+++ secao01-php-para-quem-nao-sabe-php/aula003-variaveis-tipos-de-dados-referencia/index.php
@@ -1,18 +1,40 @@
 <?php
 
+// Tipo de dados:
 // string: Tudo que pode ser colocado entre 'aspas simples' ou "aspas duplas".
 echo gettype('Erick Ferreira');
+echo "\n\n";
 
-// number: integer ou float(double)
+// number: integer ou double(float)
 echo gettype(39);
+echo "\n";
+echo gettype(10.4);
+echo "\n\n";
 
-// booleam:
-
+// boolean: true ou false
+echo gettype(10 == 10);
+echo "\n";
+echo gettype(true);
+echo "\n";
+echo gettype(false);
+echo "\n\n";
 
 // array:
+echo gettype(["text", 10, 1.2, true, []]);
+echo "\n\n";
+
+// object: Objetos.
+class Person {
+    public function teste() {}
+}
+
+echo gettype(new Person);
+echo "\n\n";
 
 
-// object:
+// null: Auxência de valor.
+echo gettype(null);
+echo "\n\n";
 
 
-// null:
+// Variáveis:
