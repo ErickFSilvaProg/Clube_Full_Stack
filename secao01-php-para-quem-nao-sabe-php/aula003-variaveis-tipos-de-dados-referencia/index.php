37dc0540d5861456d9e8b13a50aedd898f96df79
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-11-03 22:26:43 -0300

    Clube Full Stack, 03/11/2025 (noite)

---
+++ secao01-php-para-quem-nao-sabe-php/aula003-variaveis-tipos-de-dados-referencia/index.php
@@ -0,0 +1,18 @@
+<?php
+
+// string: Tudo que pode ser colocado entre 'aspas simples' ou "aspas duplas".
+echo gettype('Erick Ferreira');
+
+// number: integer ou float(double)
+echo gettype(39);
+
+// booleam:
+
+
+// array:
+
+
+// object:
+
+
+// null:
