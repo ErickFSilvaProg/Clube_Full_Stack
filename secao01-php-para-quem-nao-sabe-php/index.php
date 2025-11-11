d1f9ff08020bb60901a80ca9aeefe9849203df1a
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-11-11 11:31:44 -0300

    Clube Full Stack, 11/11/2025 (manhã)

--- secao01-php-para-quem-nao-sabe-php/index.php
+++ secao01-php-para-quem-nao-sabe-php/index.php
@@ -6,6 +6,27 @@
     <title>PHP para quem não sabe PHP</title>
 </head>
 <body>
+
+<ul>
+    <li><a href="aula002-digitar-codigo-include-require">aula002-digitar-codigo-include-require</a></li>
+    <li><a href="aula003-variaveis-tipos-de-dados-referencia">aula003-variaveis-tipos-de-dados-referencia</a></li>
+    <li><a href="aula004-constantes">aula004-constantes</a></li>
+    <li><a href="aula005-instrucao-ponto-e-virgula">aula005-instrucao-ponto-e-virgula</a></li>
+    <li><a href="aula006-operadores-aritmeticos">aula006-operadores-aritmeticos</a></li>
+    <li><a href="aula007-operadores-de-atribuicao">aula007-operadores-de-atribuicao</a></li>
+    <li><a href="aula008-operadores-incremento-decremento">aula008-operadores-incremento-decremento</a></li>
+    <li><a href="aula009-operadores-de-comparacao">aula009-operadores-de-comparacao</a></li>
+    <li><a href="aula010-operadores-logicos">aula010-operadores-logicos</a></li>
+    <li><a href="aula011-truthy-falsy">aula011-truthy-falsy</a></li>
+    <li><a href="aula012-condicionais-if-else">aula012-condicionais-if-else</a></li>
+    <li><a href="aula013-condicionais-switch">aula013-condicionais-switch</a></li>
+    <li><a href="aula014-tipos-de-dados-booleans">aula014-tipos-de-dados-booleans</a></li>
+    <li><a href="aula015-tipos-de-dados-string">aula015-tipos-de-dados-string</a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+</ul>
     
 </body>
 </html>
