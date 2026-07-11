4e13773b04f96f5d0e1bb6b2449d8e2a8304dc10
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-07-11 10:47:58 -0300

    Clube_Full_Stack 11/07/2026 (manhã)

---
+++ Secao_01_php_para_quem_nao_sabe_php/aula001_codigo_include_require/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// include 'person.php';
+// include_once 'person.php';
+
+// require 'person.php';
+require_once 'person.php';
+
+$title = 'Home';
+$lorem = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.';
+
+echo "My name is {$name}\n\n";
+echo "----> {$lorem} <----\n\n";
