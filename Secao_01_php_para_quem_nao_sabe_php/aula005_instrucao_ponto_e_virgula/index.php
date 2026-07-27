6ad798b6ead5c2590a20bd332fb868897bd843a8
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-07-27 15:15:37 -0300

    Clube_Full_Stack 27/07/2026 (tarde)

--- Secao_01_php_para_quem_nao_sabe_php/aula005_instrucao_ponto_e_virgula/index.php
+++ Secao_01_php_para_quem_nao_sabe_php/aula005_instrucao_ponto_e_virgula/index.php
@@ -1,2 +1,10 @@
 <?php
 
+$name = "Alexandre";
+
+if ($name = "Alexandre") {
+    echo "É alexandre";
+}
+echo "\n<br>";
+
+echo "Teste";
---
+++ Secao_01_php_para_quem_nao_sabe_php/aula006_operadores_aritmeticos/index.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+
+https://www.php.net/manual/pt_BR/language.operators.php
+
+    Operadores Aritméticos:
+
+        () → Parênteses
+        ** → Exponenciação
+        *  → Multiplicação
+        /  → Divisão
+        %  → Módulo
+        +  → Adição
+        -  → Subtração
+
+*/
+
+
+// Variáveis:
+$number1 = 10;
+$number2 = 20;
+$number3 = 5.5;
+
+// Exponenciação:
+echo $number1 ** 2;
+echo "<br>\n";
+
+// Multiplicação:
+echo $number1 * $number2;
+echo "<br>\n";
+
+// Divisão:
+echo $number2 / $number3;
+echo "<br>\n";
+
+// Módulo:
+echo $number1 % $number3;
+echo "<br>\n";
+
+// Adição:
+echo $number2 + $number1;
+echo "<br>\n";
+
+// Subtração:
+echo $number3 - $number1;
+echo "<br>\n";
