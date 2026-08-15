fc232d578ba6a1f040f644920c4d9ecc9bc041a9
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-08-14 22:07:52 -0300

    Clube_Full_Stack 14/08/2026 (noite)

---
+++ Exercicios_php/exercicio-004/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// 4 – Escreva um algoritmo que receba um valor, calcule e mostre para o usuário 5% e 50% deste valor.
+
+
+$valorRecebido = 100;
+$valor5Porcento = $valorRecebido * 0.05;
+$valor10Porcento = $valorRecebido * 0.10;
+
+echo $valor5Porcento;
+echo "\n";
+echo $valor10Porcento;
---
+++ Exercicios_php/exercicio-005/index.php
@@ -0,0 +1,5 @@
+<?php
+
+// 5 – Escreva um algoritmo que leia dois números, calcule o quadrado de cada um, some os quadrados e mostre o resultado.
+
+
