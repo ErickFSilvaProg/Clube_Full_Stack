914fc4e4e592d77cca9da1eabc88bc0d7b96ead6
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-08-17 16:40:07 -0300

    Clube_Full_Stack 17/08/2026 (tarde)

--- Secao_01_php_para_quem_nao_sabe_php/aula025_closures/app.php
+++ Secao_01_php_para_quem_nao_sabe_php/aula025_closures/app.php
@@ -1,22 +1,33 @@
 <?php
 
+// Toda variável que recebe uma função anônima torna-se um "objeto do tipo closure".
+
 $person = function($name) {
+    // Objeto do tipo "clasure".
     return $name;
 };
 
-echo $person('Erick');
+var_dump($person("Erick"));
 echo "\n";
 
 
-// 
-function carro() {
+function profissao($area) {
 
-    $person = function() {
-        return "Onix";
-    };
+    if ($area == "Programador") {
 
-    return $person();
+        $person = function($linguagem) use ($area) {
+
+            return "{$area} {$linguagem}";
+        };
+
+        return $person("PHP");
+
+    }
+    else {
+
+        return "Outra profissão";
+    }
 
 }
 
-var_dump(carro());
+var_dump(profissao("Programador"));
