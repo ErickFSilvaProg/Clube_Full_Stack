309d0905fece4d9c0a2d435ce65e6ab3c68f9254
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-08-28 16:43:03 -0300

    Clube_Full_Stack 28/08/2026 (tarde)

--- Secao_01_php_para_quem_nao_sabe_php/aula026_callbacks/program.php
+++ Secao_01_php_para_quem_nao_sabe_php/aula026_callbacks/program.php
@@ -11,11 +11,15 @@
         3. call_user_func (Espera um callback como parâmetro).
             ↪ Chama uma função e seu(s) parâmetro(s).
             ↪ Chama o primeiro parâmetro como array, caso ele seja um objeto, com método estático ou não e queira usar um método dele.
+        
+        4. call_user_func dentro de outras funções.
+
+        5. __invoke (método mágico) pode ser passado como callback.
 
 */
 
 
-// Etapa 1 ***************************:
+// Etapa 1: callback simples ***********************
 function saudacao($nome) {
     return 'Olá, meu nome é ' . $nome;
 }
@@ -28,7 +32,7 @@ echo executor('saudacao');
 echo "\n";
 
 
-// Etapa 2 ***************************:
+// Etapa 2: Verificar se é callback com is_callable ***********************
 function exibeCarro($carro) {
     return "Carro escolhido: {$carro}";
 }
@@ -47,14 +51,61 @@ function executor2($callback2) {
 
 $addCarro = 'exibeCarro';
 echo executor2($addCarro);
-echo "\n";
+echo "\n\n";
 
 
-// Etapa 3 ***************************:
+// Etapa 3: call_user_func ***********************
+// Opção 1:
 function exibeMunicipio($municipio) {
     return "Moro no município de {$municipio}";
 }
 
-// É utilizado para passar um callback com os parâmetros da função.
-echo call_user_func('exibeMunicipio','Garanhuns');
-echo "\n";
+$nomeCidade = readline('Informe a cidade que mora: ');
+echo call_user_func('exibeMunicipio', $nomeCidade);
+echo "\n\n";
+
+
+// Opção 2:
+class User {
+    public function coletaNome($nome, $nascido) {
+        return "Olá, meu nome é {$nome} e nasci em {$nascido}.";
+    }
+}
+
+$user = new User;
+$nome = readline("Informe seu nome: ");
+$nascido = readline('Você nasceu em que ano? ');
+
+echo call_user_func([$user, 'coletaNome'], $nome, $nascido);
+echo "\n\n";
+
+
+// Etapa 4: call_user_func dentro de outras funções ***********************
+function coletaAnoAtual($anoAtual) {
+    return $anoAtual;
+}
+
+function executora4($callback4) {
+    $anoAtual = readline('Em que ano estamos: ');
+    return 'Estamos no ano de ' . call_user_func($callback4, $anoAtual);
+}
+
+echo executora4('coletaAnoAtual');
+echo "\n\n";
+
+
+// Etapa 5: __invoke pode ser passado como callback ***********************
+class User5 {
+    public function __invoke() {
+        return 'Função __invoke';
+    }
+}
+
+$user = new User5;
+
+function executora5($callback5) {
+    return $callback5();
+}
+
+echo executora5($user);
+echo "\n\n";
--- Secao_01_php_para_quem_nao_sabe_php/revisao.php
+++ Secao_01_php_para_quem_nao_sabe_php/revisao.php
@@ -1,14 +1,2 @@
 <?php
 
-$listaCidade = [];
-
-function addCidade($listaCidade, $cidade) {
-    array_push($listaCidade, $cidade);
-    return $listaCidade;
-}
-
-$listaCidade = call_user_func('addCidade', $listaCidade, 'Garanhuns');
-$listaCidade = call_user_func('addCidade', $listaCidade, 'Caruaru');
-$listaCidade = call_user_func('addCidade', $listaCidade, 'Recife');
-
-print_r($listaCidade);
